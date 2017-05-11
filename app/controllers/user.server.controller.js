0f7e8152fc277536b973da29aaabb7e463a6b695
phumai <kang081@hotmail.com>
2017-05-12 00:50:26 +0700

    add query data

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -1,5 +1,6 @@
 var firebase = require("firebase");
 var cnt =0;
+ 
     firebase.initializeApp({
         serviceAccount: "./nodemon-432721ff479b.json",
         databaseURL: "https://nodemon-39e5c.firebaseio.com/"
@@ -29,12 +30,23 @@ exports.insertAgain = function(req,res){
             IsInsert:false
     });
 };
+var childsnap={};
+var data ={}
+var obj={};
 exports.show = function(req,res){
-   ref.once('value')
-   .then(function(snap){
-     console.log(snap.val());
-     res.render('list',{
-        para:snap.val()
-     });
-   });
+    
+    
+ref.on('value',function(snapshot){
+      snapshot.forEach(function(childsnap) {
+          console.log('loop',childsnap.val());
+          data= JSON.stringify(childsnap.val());
+          
+      });
+  });
+  console.log('data',data);
+  obj = JSON.parse(data);
+  console.log('obj',obj.text);
+        res.render('list',{
+            para:'The last insert is [ '+obj.text +' ]'
+        });
 };
